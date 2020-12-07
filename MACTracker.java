75ffc2247d81256521162f68481772c65c11b6c7
Jesse Huang <jesse.huang1205@gmail.com>
2020-12-07 17:51:16 -0500

    Add files via upload

--- MACTracker.java
+++ MACTracker.java
@@ -2,16 +2,21 @@ package net.floodlightcontroller.mactracker;
  
 import java.util.Collection;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
  
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.OFType;
 import org.projectfloodlight.openflow.types.MacAddress;
+import org.projectfloodlight.openflow.types.EthType;
+import org.projectfloodlight.openflow.types.VlanVid;
+import org.projectfloodlight.openflow.types.*;
  
 import net.floodlightcontroller.core.IFloodlightProviderService;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.Set;
-import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,12 +27,18 @@ import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.core.*;
  
 public class MACTracker implements IOFMessageListener, IFloodlightModule {
  
     protected IFloodlightProviderService floodlightProvider;
     protected Set<Long> macAddresses;
+    protected Set<String> ipaddress;
     protected static Logger logger;
+    protected int count = 0;
+    protected boolean triggered = false;
+    Timer timer = new Timer();
+    TimerTask task = new Helper(); 
 
     @Override
     public String getName() {
@@ -72,25 +83,115 @@ public class MACTracker implements IOFMessageListener, IFloodlightModule {
 
         floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
         macAddresses = new ConcurrentSkipListSet<Long>();
+        ipaddress = new ConcurrentSkipListSet<String>();
         logger = LoggerFactory.getLogger(MACTracker.class);
+        timer.schedule(task, 9000,9000);
 
     }
  
     @Override
     public void startUp(FloodlightModuleContext context) {
 
+        logger.info("mactracker started");
+        System.out.println("-------------------Started--------------------");
         floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
 
     }
+
+    public void anom(){
+
+        if(!triggered){
+            logger.info("Anomoly detected.");
+            triggered = true;
+        }
+
+    }
  
+    class Helper extends TimerTask 
+    { 
+        public void run() 
+        { 
+             triggered = false;
+             count = 0;
+             ipaddress.clear();
+        } 
+    }
+
     @Override
-    public net.floodlightcontroller.core.IListener.Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
-        Ethernet eth =IFloodlightProviderService.bcStore.get(cntx,IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
-            Long sourceMACHash = eth.getSourceMACAddress().getLong();
-            if (!macAddresses.contains(sourceMACHash)) {
-                macAddresses.add(sourceMACHash);
-                logger.info("MAC Address: {} seen on switch: {}",eth.getSourceMACAddress().toString(),sw.getId().toString());
+    public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+        switch (msg.getType()) {
+        case PACKET_IN:
+            /* Retrieve the deserialized packet in message */
+            Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+    
+            /* Various getters and setters are exposed in Ethernet */
+            MacAddress srcMac = eth.getSourceMACAddress();
+            VlanVid vlanId = VlanVid.ofVlan(eth.getVlanID());
+    
+            /* 
+            * Check the ethertype of the Ethernet frame and retrieve the appropriate payload.
+            * Note the shallow equality check. EthType caches and reuses instances for valid types.
+            */
+            if (eth.getEtherType() == EthType.IPv4) {
+                /* We got an IPv4 packet; get the payload from Ethernet */
+                IPv4 ipv4 = (IPv4) eth.getPayload();
+                
+                /* Various getters and setters are exposed in IPv4 */
+                byte[] ipOptions = ipv4.getOptions();
+                IPv4Address dstIp = ipv4.getDestinationAddress();
+                
+                /* 
+                * Check the IP protocol version of the IPv4 packet's payload.
+                */
+                if (ipv4.getProtocol() == IpProtocol.TCP) {
+                    /* We got a TCP packet; get the payload from IPv4 */
+                    TCP tcp = (TCP) ipv4.getPayload();
+    
+                    /* Various getters and setters are exposed in TCP */
+                    TransportPort srcPort = tcp.getSourcePort();
+                    TransportPort dstPort = tcp.getDestinationPort();
+                    short flags = tcp.getFlags();
+                    
+                    /* Your logic here! */
+                    //logger.info(String.valueOf(flags));
+                    if(!ipaddress.contains(ipv4.getSourceAddress().toString())){
+
+                        ipaddress.add(ipv4.getSourceAddress().toString());
+                        count++;
+
+                    }
+                    /*if(flags == 511){
+                        anom();
+                    }*/
+                    if(count >= 1000){
+                        anom();
+                    }
+
+                } else if (ipv4.getProtocol() == IpProtocol.UDP) {
+                    /* We got a UDP packet; get the payload from IPv4 */
+                    UDP udp = (UDP) ipv4.getPayload();
+    
+                    /* Various getters and setters are exposed in UDP */
+                    TransportPort srcPort = udp.getSourcePort();
+                    TransportPort dstPort = udp.getDestinationPort();
+                    
+                    /* Your logic here! */
+                }
+    
+            } else if (eth.getEtherType() == EthType.ARP) {
+                /* We got an ARP packet; get the payload from Ethernet */
+                ARP arp = (ARP) eth.getPayload();
+    
+                /* Various getters and setters are exposed in ARP */
+                boolean gratuitous = arp.isGratuitous();
+    
+            } else {
+                /* Unhandled ethertype */
             }
+            break;
+        default:
+            break;
+        }
         return Command.CONTINUE;
     }
  
